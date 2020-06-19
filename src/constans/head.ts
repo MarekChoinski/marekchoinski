7b0f3eaf3a9ce80e39aa268cb19106a70e74b3d4
Marek Choiński <marekchoinski17@gmail.com>
2020-06-19 17:07:22 +0200

    Added head component

---
+++ src/constans/head.ts
@@ -0,0 +1,7 @@
+export const head = {
+    title: "Marek Choiński",
+    description: "I'm (really good) frontend developer.",
+    keywords: "marek,choiński,choinski,web,fontend,developer",
+    url: "https://marekchoinski.com/",
+    // image: '/thumbnail.png',
+};
