f519d3441ed15b1de90a940d368caddfc58e3dac
Marek Choiński <marekchoinski17@gmail.com>
2020-06-23 14:56:00 +0200

    Updated head

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -11,7 +11,6 @@ const Head: React.FC = () => {
             <title>{title}</title>
             <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap" rel="stylesheet" />
             <meta property="og:type" content="website" />
-            <meta name="description" content={description} />
             <meta property="og:title" content={title} />
             <meta name="description" content={description} />
             <meta name="keywords" content={keywords} />
