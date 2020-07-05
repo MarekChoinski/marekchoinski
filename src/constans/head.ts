42a57916599b4bac67871f3e4e51c7416554c4b9
Marek Choiński <marekchoinski17@gmail.com>
2020-07-05 21:17:13 +0200

    Fixed typo

--- src/constans/head.ts
+++ src/constans/head.ts
@@ -1,6 +1,6 @@
 export const head = {
     title: "Marek Choiński",
-    description: "I'm (really good) frontend developer.",
+    description: "I'm a (really good) frontend developer.",
     keywords: "marek,choiński,choinski,web,fontend,developer",
     url: "https://marekchoinski.com/",
     // image: '/thumbnail.png',
