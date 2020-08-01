992aeef7e4aa734c08b3278cfb43576d2b003648
Marek Choiński <marekchoinski17@gmail.com>
2020-08-01 15:44:25 +0200

    Added image scss

---
+++ src/pages/consquare.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+// import '../styles/index.scss'
+import Consquare from "../views/consquare/Consquare"
+
+export default function Home() {
+  return <Consquare />
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+import '../styles/index.scss'
+import App from '../views/mainpage/App'
+
+export default function Home() {
+  return <App />;
+}
