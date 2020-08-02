9c0e0688abd49c5bc86e55bf88f49fc791d7b793
Marek Choiński <marekchoinski17@gmail.com>
2020-08-02 16:22:48 +0200

    Added tiles

--- src/views/consquare/WizardTower.tsx
+++ src/views/consquare/WizardTower.tsx
@@ -1,19 +1,61 @@
 import React from "react"
 import TowerVideo from "../../static/consquare/tower.mp4";
 
+import BuildIcon from "../../assets/images/consquare/backpack.png";
+
 const WizardTower: React.FC = () => {
 
     return (
-        <section className="infoTile infoTile--build">
+        <section className="wizardTower">
             <div className="towerVideo">
                 <video autoPlay loop muted playsInline width="100%" height="100%">
                     <source src={TowerVideo} type="video/mp4" />
                 </video>
             </div>
-            <div className="infoTile infoTile--build">w</div>
-            <div className="infoTile infoTile--fight">e</div>
-            <div className="infoTile infoTile--collect">r</div>
-            <div className="infoTile infoTile--explore">t</div>
+            <div className="infoTile infoTile--build">
+                <img src={BuildIcon} alt="Build section icon" className="infoTile__icon" />
+                <div className="infoTile__infoContainer">
+                    <header className="infoTile__title">Build</header>
+                    <p className="infoTile__description">
+                        Protect your castle walls with magical fortifications and develop
+                        your guild by improvements gained by mining. Grow your empire to
+                        unimaginable dimensions by creating impressive castles and various fortresses.
+                </p>
+                </div>
+            </div>
+            <div className="infoTile infoTile--fight">
+                <img src={BuildIcon} alt="Build section icon" className="infoTile__icon" />
+                <header className="infoTile__title">Fight</header>
+                <div className="infoTile__infoContainer">
+                    <p className="infoTile__description">
+                        Gather your friends for your team and become the most powerful guild in the area.
+                        Train your team's tactics on the terrifying creatures that live in the world of Consquare.
+                        Lead your army in mighty battles right outside your house.
+                </p>
+                </div>
+            </div>
+            <div className="infoTile infoTile--collect">
+                <img src={BuildIcon} alt="Build section icon" className="infoTile__icon" />
+                <div className="infoTile__infoContainer">
+                    <header className="infoTile__title">Collect</header>
+                    <p className="infoTile__description">
+                        Place mines on extensive procedurally generated random structures. Use items found during
+                        your wander to develop your character. Collect necessary ores for your team and found all
+                        of mystical crystals. Become more and more powerful each day.
+                </p>
+                </div>
+            </div>
+            <div className="infoTile infoTile--explore">
+                <img src={BuildIcon} alt="Build section icon" className="infoTile__icon" />
+                <div className="infoTile__infoContainer">
+                    <header className="infoTile__title">Explore</header>
+                    <p className="infoTile__description">
+                        The world of Consquare pretty resembles our world, but is steeped in enigmatic wizardy.
+                        In vast corners of the world you will find many extraordinary characters that will change
+                        your outlook on life. The openness of our game world is limited only by the boundaries of our Earth. globe.
+                </p>
+                </div>
+            </div>
         </section>
 
     );
